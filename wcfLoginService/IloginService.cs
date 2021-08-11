32b17f85b98f6755ce66e9ab1426430815de71da
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-08-10 21:49:06 -0600

    Correcciones en nombre de servicio

--- wcfLoginService/IloginService.cs
+++ wcfLoginService/IloginService.cs
@@ -9,9 +9,9 @@ namespace wcfLoginService
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IloginService" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
-    public interface IloginService
+    public interface ILoginService
     {
         [OperationContract]
-        bool validateLogin(string user, string pass);
+        bool ValidateLogin(string user, string pass);
     }
 }
--- wcfLoginService/loginService.svc.cs
+++ wcfLoginService/loginService.svc.cs
@@ -9,9 +9,9 @@ namespace wcfLoginService
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "loginService" en el código, en svc y en el archivo de configuración a la vez.
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione loginService.svc o loginService.svc.cs en el Explorador de soluciones e inicie la depuración.
-    public class loginService : IloginService
+    public class LoginService : ILoginService
     {
-        public bool validateLogin(string user, string pass)
+        public bool ValidateLogin(string user, string pass)
         {
             bool succed = false;
             if (user=="fernando.estrada@tedsoluciones.com" && pass == "12345678")
